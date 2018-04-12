7383cd3f6b2e730a8eeecfe727171ae51ae4126c
albornoz <albornoz_13-25@hotmail.com>
2018-04-12 14:48:02 +0200

    organizacion

--- printf/pesetas a euros/cantidad.cpp
+++ printf/pesetas a euros/cantidad.cpp
@@ -5,15 +5,15 @@
 int main(){
 int pesetas;
 float euros;
-float euroo;
-int peseta;
+/*float euroo;
+int peseta;*/
   printf(" \n Dime una cantidad en pesetas: ");
   scanf(" %d", &pesetas);
   euros = pesetas / euro;
   printf(" \n equivale a: %.2f euros ", euros);
-  printf(" \n Dime una cantidad en euros: ");
+/*  printf(" \n Dime una cantidad en euros: ");
   scanf(" %d", &euroo);
   peseta = euro/ peseta;
-  printf(" \n equivale a: %.2f pesetas ", peseta);
+  printf(" \n equivale a: %.2f pesetas ", peseta);*/
       return EXIT_SUCCESS;
 }
